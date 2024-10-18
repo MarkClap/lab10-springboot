6942ed0635f1da5839da2634e1ac6712b624dfdb
MarkClap <blusito48@gmail.com>
2024-10-18 11:11:49 -0500

    configuracion

---
+++ src/main/java/com/tecsup/petclinic/exception/PetNotFoundException.java
@@ -0,0 +1,25 @@
+package com.tecsup.petclinic.exception;
+
+import java.io.Serial;
+
+/**
+ * 
+ * @author jgomezm
+ *
+ */
+public class PetNotFoundException extends Exception {
+
+	/**
+	 * 
+	 */
+	@Serial
+	private static final long serialVersionUID = 1L;
+
+	public PetNotFoundException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	
+	
+}
